f9fa66ae6ec6ec7d8c8cf6ff6b7104022e1d5352
jiahoe <mrjiahoe@hotmail.com>
2022-05-18 13:46:52 +0800

    fine tuning

--- main.js
+++ main.js
@@ -8,14 +8,27 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
-const N = 100;
+const N = 500;
 const cars = generateCars(N);
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
-	bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+	for (let i = 0; i < cars.length; i++) {
+		cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+		if (i != 0) {
+			NeuralNetwork.mutate(cars[i].brain, 0.1);
+		}
+	}
 }
 
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
+const traffic = [
+	new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2, getRandomColor()),
+	new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+	new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+	new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+	new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+	new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2, getRandomColor()),
+	new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2, getRandomColor()),
+];
 
 animate();
 
@@ -42,7 +55,7 @@ function animate(time) {
 	for (let i = 0; i < cars.length; i++) {
 		cars[i].update(road.borders, traffic);
 	}
-	const bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
+	bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
 
 	carCanvas.height = window.innerHeight;
 	networkCanvas.height = window.innerHeight;
@@ -52,14 +65,14 @@ function animate(time) {
 
 	road.draw(carCtx);
 	for (let i = 0; i < traffic.length; i++) {
-		traffic[i].draw(carCtx, "red");
+		traffic[i].draw(carCtx);
 	}
 	carCtx.globalAlpha = 0.2;
 	for (let i = 0; i < cars.length; i++) {
-		cars[i].draw(carCtx, "blue");
+		cars[i].draw(carCtx);
 	}
 	carCtx.globalAlpha = 1;
-	bestCar.draw(carCtx, "blue", true);
+	bestCar.draw(carCtx, true);
 
 	carCtx.restore();
 
--- utils.js
+++ utils.js
@@ -44,5 +44,10 @@ function getRGBA(value) {
 	const R = value < 0 ? 0 : 255;
 	const G = R;
 	const B = value > 0 ? 0 : 255;
-	return "rgba(" + R + ", " + G + ", " + B + ", " + alpha + ")";
+	return "rgba(" + R + "," + G + "," + B + "," + alpha + ")";
+}
+
+function getRandomColor() {
+	const hue = 290 + Math.random() * 260;
+	return "hsl(" + hue + ", 100%, 60%)";
 }
